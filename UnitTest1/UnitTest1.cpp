8044880a741a872a56eb97a92ebbea70f398932c
{Vlad} <{vladpolikarpovvl@gmail.com}>
2022-06-13 15:27:02 +0300

    first commit

---
+++ UnitTest1/UnitTest1.cpp
@@ -0,0 +1,22 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../../../../../політех/ооп/7/lab_7.3/lab_7.3/lab_7.3.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest1
+{
+	TEST_CLASS(UnitTest1)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			string text = "std set is an associative container, that contains a sorted set of unique objects of type Key.!";
+
+			set<char> s = find(text);
+
+			Assert::AreEqual(s.size(), (size_t)3);
+		}
+	};
+}
---
+++ lab_7.3/lab_7.3.cpp
@@ -0,0 +1,43 @@
+﻿#include <iostream>
+#include <string>
+#include <set>
+
+using namespace std;
+
+set<char> find(string text) {
+	set<char> result;
+
+	set<char> filter = { ',', '.', ':', ';', '!', '?' };
+
+	for (char c : filter) {
+
+		int n = count(text.begin(), text.end(), c);
+
+		if (n == 1) result.insert(c);
+
+	}
+
+	return result;
+}
+
+
+
+int main() {
+	string text = "std set is an associative container, that contains a sorted set of unique objects of type Key.!";
+
+	set<char> s = find(text);
+
+	cout << "text: " << text << endl;
+
+	cout << "set: { ";
+
+	for (char c : s) {
+		cout << c << " ";
+	}
+
+	cout << "}" << endl;
+
+	cout << "size: " << s.size() << endl;
+
+	return 0;
+}
